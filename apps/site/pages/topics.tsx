260a73068c8666be3ba5b22a8a3e5576c4b4a0b7
thundermiracle <cissoliu@gmail.com>
2022-05-21 18:42:29 +0900

    feat: add shared-ui library

---
+++ apps/site/pages/topics.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TopicButton } from '@study-nx/shared/ui';
+
+const Topics = () => {
+  return (
+    <div>
+      <TopicButton />
+    </div>
+  );
+};
+
+export default Topics;
---
+++ libs/shared/ui/tailwind.config.js
@@ -0,0 +1,14 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  presets: [require('../../../tailwind.preset')],
+};
